b6888f89e6c6e3e2518845e8303b8393532481cb
Ajaysai157 <bhargavchonga976@gmail.com>
2026-03-05 11:29:16 +0530

    Updated script and plots

--- scripts/Week7-script.R
+++ scripts/Week7-script.R
@@ -14,7 +14,24 @@ par(mfrow=c(1,2))
 mosaicplot(HairEyeColor[,,"Male"],main="Male",color=TRUE)
 mosaicplot(HairEyeColor[,,"Female"],main="Female",color=TRUE)
 par(mfrow=c(1,1))
+mosaicplot(HairEye2D,col=c("brown","black","red","yellow"),)
+main="Hair vs Eye(Custom colors)"
+
 
 install.packages("vcd")
 library(vcd)
 mosaic(HairEyeColor,shape=TRUE,legend=TRUE,main="Shaped Mosaic Plot")
+
+hair_eye_df <-as.data.frame(HairEyeColor)
+head(hair_eye_df)
+
+library(ggplot2)
+
+ggplot(hair_eye_df,aes(x=Hair,y=Freq,fill=Eye))+
+  geom_bar(stat="identity",position="fill")+facet_wrap(~Sex)+
+  labs(
+    title="Proportional Distribution of Eye Color by Hair",
+    y="Proportion"
+  )+theme_minimal()
+
+mosaic(~Hair+Eye+Sex,data=hair_eye_df,shade=TRUE,legend=TRUE)
