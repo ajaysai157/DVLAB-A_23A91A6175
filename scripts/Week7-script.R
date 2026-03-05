23c534af58666501a02e08dd7cbd0d3875b17424
Ajaysai157 <bhargavchonga976@gmail.com>
2026-03-05 10:53:23 +0530

    week7 program upto 3 plots

---
+++ scripts/Week7-script.R
@@ -0,0 +1,15 @@
+data(HairEyeColor)
+str(HairEyeColor)
+View(HairEyeColor)
+
+?HairEyeColor
+
+mosaicplot(HairEyeColor)
+mosaicplot(HairEyeColor, main="Mosaic Plot: Hair,Eye Color and sex",color = TRUE)
+
+HairEye2D <- margin.table(HairEyeColor,c(1,2))
+mosaicplot(HairEye2D,main = "Hair vs Eye Color Mosaic Plot",color = TRUE)
+
+par(mfrow=c(1,2))
+mosaicplot(HairEyeColor[,,"Male"],main="Male",color=TRUE)
+mosaicplot(HairEyeColor[,,"Female"],main="Female",color=TRUE)
